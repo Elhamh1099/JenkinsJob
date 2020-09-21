8b6dde4c6e3cf38037bbc8913434e400d5330f63
unknown <Elhamh1099@gmail.com>
2020-09-21 12:35:32 -0400

    Jenkins Job

---
+++ src/main/java/MoreGitPractice/MoreGitPractice/App.java
@@ -0,0 +1,13 @@
+package MoreGitPractice.MoreGitPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
